186da3eea86273bd5381916686b307146bd27431
kuncoroadjie <kuncoroadjie17@gmail.com>
2022-07-09 12:35:52 +0700

    add target dzikir feature

--- function.js
+++ function.js
@@ -1,11 +1,12 @@
 function openDialog(){
-    document.getElementById("dialog").showModal();
+    document.getElementById("modal").classList.add("modal-active");
 }
-function closeDialog(){
-    document.getElementById("dialog").close();
+function closeModal(){
+    document.getElementById("modal").classList.remove("modal-active");
 }
+var totalTarget = 0;
 var totalLafadz = 0;
-var status = "mati";
+// var status = "mati";
 function runSpeechRecognition() {  		    	           
     // get action element reference
     var action = document.getElementById("action");
@@ -32,20 +33,16 @@ function runSpeechRecognition() {
     recognition.onresult = function(event) {
         var transcript = event.results[0][0].transcript;               
         var substr_count = transcript.match(/astaghfirullahaladzim|astagfirullah|subhanallah|alhamdulillah|allahuakbar|allahu akbar|lailahaillallah|lailahailallah/gi).length;
-        console.log(transcript);
-        console.log(substr_count);
-        console.log("Kuncoro") 
-        if(substr_count >0){
+        if(substr_count > 0){
             countLafadz(substr_count);
         }
-        
     };
   
      // start recognition
      recognition.lang ='id-ID';
      recognition.start();
 }
-// function help(){
+// function speak(){
 //     let syth = window.speechSynthesis;
 //     let utterance = new SpeechSynthesisUtterance();
 //     utterance.lang = "id-ID";
@@ -70,8 +67,40 @@ function finishMsg(){
 function countLafadz(substr_count){
     totalLafadz = totalLafadz + substr_count;
     document.getElementById("counter").innerHTML = totalLafadz; 
+    if(totalTarget == 0 || totalLafadz < totalTarget ){
+        $('.btn-mulai').removeClass('btn-disable').prop('disabled', false);
+    }else{
+        setTimeout(finishTarget, 300);
+    }
+    
 }
 function resetCounter(){
     totalLafadz=0;
     document.getElementById("counter").innerHTML = 0; 
+    $('.btn-mulai').removeClass('btn-disable').prop('disabled', false);
+    document.getElementById("target-count").innerHTML = "-";
+}
+function target(){
+    $('.btn-mulai').removeClass('btn-disable').prop('disabled', false);
+    let jumlahDzikir = prompt("Masukkan jumlah dzikir", 100);
+        if (jumlahDzikir == null || jumlahDzikir == "") {
+            
+        }else if(isNaN(jumlahDzikir)) {
+            window.alert("Input harus angka");
+        }else if(jumlahDzikir<=0 ){
+            window.alert("Masukkan angka lebih dari 0");
+        }else if(jumlahDzikir<=totalLafadz){
+            window.alert("Angka harus lebih dari "+totalLafadz);
+        }else{
+            this.totalTarget = jumlahDzikir;
+            document.getElementById("target-count").innerHTML = totalTarget;
+            $('.btn-mulai').removeClass('btn-disable').prop('disabled', false);
+        }
+        
+}
+function finishTarget(){
+    if(totalTarget<=totalLafadz){
+        window.alert("Target dzikir sebanyak "+totalTarget+" telah tercapai");
+    }
+    $('.btn-mulai').addClass('btn-disable').prop('disabled', true);
 }
